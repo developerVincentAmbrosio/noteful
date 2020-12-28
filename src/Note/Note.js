e257958fa6afded705ccf880585b4e22199ea1bc
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2020-12-28 16:48:04 -0500

    worked on checkpoint

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,30 +2,69 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ApiContext from '../ApiContext'
+import config from '../config'
+//import './Note.css'
+
+export default class Note extends React.Component {
+  static defaultProps ={
+    onDeleteNote: ()=>{},
+  }
+  static contextType = ApiContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteID = this.props.id
+
+  fetch(`${config.API_ENDPOINT}/notes/${noteID}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(res=> {
+      if(!res.ok)
+        return res.json().then(e => Promise.reject(e))
+      return res.json()
+    })
+    .then(()=> {
+      this.context.deleteNote(noteId)
+      this.props.onDeleteNote(noteId)
+    })
+    .catch(error => {
+      console.error({ error });
+    })
+  }
 
 
-export default function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
+  render () {
+    const { name, id, modified } = this.props
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {name}
+          </Link>
+        </h2>
+        <button
+          className='Note__delete'
+          type='button'
+          onClick={this.handleClickDelete}
+        >
+          <FontAwesomeIcon icon='trash-alt' />
           {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {format ? (props.modified, 'DD MMM YYYY') : null}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
