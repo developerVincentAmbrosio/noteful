499ddd9c93a8290187dbc70c975dd6b744b5657f
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-03-24 20:44:18 -0400

    changed config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
+const config = {
+  API_ENDPOINT: 'https://fast-ocean-41904.herokuapp.com/',
+};
 
-export default {
-    API_ENDPOINT: 'https://fast-ocean-41904.herokuapp.com/',
-  }
+export default config
